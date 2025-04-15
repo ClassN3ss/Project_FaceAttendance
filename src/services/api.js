43f2c24e7c28cad1c002f59fccbb2acfd71d29f3
PathaninThima <nattawut.thima@gmail.com>
2025-04-16 05:12:47 +0700

    editapi

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use(
   (config) => {
     const token = sessionStorage.getItem('token');
-    console.log("Sending token:", token); // ✅ Debug ตรงนี้
+    console.log("Sending token:", token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
